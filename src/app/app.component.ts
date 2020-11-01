1d8750c1017da13c69c9ffecfc89695af26116f6
Sergey-Klimenko <s.i.klimenko.work@gmail.com>
2020-11-01 07:21:08 +0300

    #1 complete draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription, interval } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +11,46 @@ export class AppComponent implements OnInit, OnDestroy {
   title = 'exchange-rates';
   public stream$: Observable<number>;
   private subscriptions: Subscription[] = [];
+  sourceUrls = [
+    'https://www.cbr-xml-daily.ru',
+    'https://www.cbr-xml-daily.ru/daily_utf8.xml',
+    'https://www.cbr-xml-daily.ru/daily_json.js'
+  ];
+  currentSourceNumber = 0;
+
+  constructor(
+    private appService: AppService
+  ) {
+  }
 
   ngOnInit(): void {
-    this.stream$ = interval(1000);
+    this.interrogateSources();
+    this.stream$ = interval(10000);
     this.subscriptions.push(
       this.stream$.subscribe(
-        value => console.log(value)
+        () => this.interrogateSources()
       )
     );
   }
 
   ngOnDestroy(): void {
     this.subscriptions.forEach(item => item.unsubscribe());
+  }
+
+  interrogateSources(): void {
+    const sub = this.appService.getData(this.sourceUrls[this.currentSourceNumber]).subscribe((result) => {
+        console.log(result);
+        // console.log('успех this.sourceUrls[this.currentSourceNumber]', this.sourceUrls[this.currentSourceNumber]);
+        this.currentSourceNumber = 0;
+        sub.unsubscribe();
+      },
+      (err) => {
+        console.log(err);
+        console.log('this.sourceUrls[this.currentSourceNumber]', this.sourceUrls[this.currentSourceNumber]);
+        this.currentSourceNumber++;
+        this.interrogateSources();
+      });
+
   }
 
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  public getData(url: string): Observable<any> {
+    return this.http.get(url);
+  }
 }
